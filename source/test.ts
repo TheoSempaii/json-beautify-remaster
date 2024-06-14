1c406164909456557312ddfcd09a41a25864a698
@TheoSempaii <61527540+TheoSempaii@users.noreply.github.com>
2024-06-14 16:50:11 -0300

    changed to typescrypt

---
+++ source/test.ts
@@ -0,0 +1,18 @@
+import beautify from "."; "./index"
+
+console.log(beautify(
+  {
+    str: "Hello World",
+    num: 42,
+    smallarray: [1, 2, 3, "foo", {}],
+    smallobject: { foo: "bar", bar: 42 },
+    bigarray: [1, 2, 3, "foo", { foo: "bar", bar: 42, arr: [1, 2, 3, "foo", {}] }],
+    bigobject: { foo: [1, 2, 3, "foo", {}], bar: 42, a: { b: { c: 42 } }, foobar: "FooBar" },
+    uwu: () => {
+      console.log("Hola");
+    }
+  },
+  null,
+  2,
+  process.argv[2] ? parseInt(process.argv[2], 10) : 80
+));
